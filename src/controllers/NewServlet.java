6f1c1e250ee755384eb7c3ee728a25cbdb814425
oiwa1108 <gamma.1108@gmail.com>
2023-08-16 00:35:21 +0900

    NewServlet.javaを作成

---
+++ src/controllers/NewServlet.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class NewServlet
+ */
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NewServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+        em.getTransaction().begin();
+
+        Task task = new Task();
+
+        String content = "ホームページ原案作成";
+        task.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        task.setCreated_at(currentTime);
+        task.setUpdated_at(currentTime);
+
+        em.persist(task);
+        em.getTransaction().commit();
+
+     // 自動採番されたIDの値を表示
+        response.getWriter().append(Integer.valueOf(task.getId()).toString());
+
+        em.close();
+    }
+}
